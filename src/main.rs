95ff076edb5f2cfa9f299de1eac221ec0a2a4711
esposm03 <36164633+esposm03@users.noreply.github.com>
2020-10-07 22:35:03 +0200

    Day 1 - Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use std::io::Read;
+use std::fs::read_to_string;
 
 fn main() {
-    let mut sum = 0.0;
-    let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input).unwrap();
+    let module_mass: f32 = 14.0;
+    let mut sum: f32 = 0.0;
+        let mut fuel_mass = (module_mass / 3.0).floor() - 2.0;
+        while fuel_mass > 0.0 {
+            sum += fuel_mass;
+            fuel_mass = (fuel_mass / 3.0).floor() - 2.0;
+        }
 
-    for line in input.lines() {
-        let num = line.parse::<f32>().unwrap();
-        sum += (num / 3.0).floor() - 2.0;
+    println!("{}", sum);
+
+    let mut sum = 0.0;
+    let input = read_to_string("input").unwrap();
+
+    for module_mass in input.lines().map(|line| line.parse::<f32>().unwrap()) {
+        let mut fuel_mass = (module_mass / 3.0).floor() - 2.0;
+        while fuel_mass > 0.0 {
+            sum += fuel_mass;
+            fuel_mass = (fuel_mass / 3.0).floor() - 2.0;
+        }
     }
 
     println!("{}", sum);
