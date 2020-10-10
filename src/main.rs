6322b6775c114a067bb364773bcab894afe81386
esposm03 <36164633+esposm03@users.noreply.github.com>
2020-10-10 21:57:19 +0200

    Day 4 - Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,55 @@
-use rayon::prelude::*;
+use itertools::Itertools;
 
 fn main() {
-    let input = "R1003,U741,L919,U341,L204,U723,L113,D340,L810,D238,R750,U409,L104,U65,R119,U58,R94,D738,L543,U702,R612,D998,L580,U887,R664,D988,R232,D575,R462,U130,L386,U386,L217,U155,L68,U798,R792,U149,L573,D448,R76,U896,L745,D640,L783,D19,R567,D271,R618,U677,L449,D651,L843,D117,L636,U329,R484,U853,L523,U815,L765,U834,L500,U321,R874,U90,R473,U31,R846,U549,L70,U848,R677,D557,L702,U90,R78,U234,R282,D289,L952,D514,R308,U255,R752,D338,L134,D335,L207,U167,R746,U328,L65,D579,R894,U716,R510,D932,L396,U766,L981,D115,L668,U197,R773,U898,L22,U294,L548,D634,L31,U626,R596,U442,L103,U448,R826,U511,R732,U680,L279,D693,R292,U641,R253,U977,R699,U861,R534,D482,L481,U929,L244,U863,L951,D744,R775,U198,L658,U700,L740,U725,R286,D105,L629,D117,L991,D778,L627,D389,R942,D17,L791,D515,R231,U418,L497,D421,L508,U91,R841,D823,L88,U265,L223,D393,L399,D390,L431,D553,R40,U724,L566,U121,L436,U797,L42,U13,R19,D858,R912,D571,L207,D5,L981,D996,R814,D918,L16,U872,L5,U281,R706,U596,R827,D19,R976,D664,L930,U56,R168,D892,R661,D751,R219,U343,R120,U21,L659,U976,R498,U282,R1,U721,R475,D798,L5,U396,R268,D454,R118,U260,L709,D369,R96,D232,L320,D763,R548,U670,R102,D253,L947,U845,R888,D645,L734,D734,L459,D638,L82,U933,L485,U235,R181,D51,L45,D979,L74,D186,L513,U974,R283,D493,R128,U909,L96,D861,L291,U640,R793,D712,R421,D315,L152,U220,L252,U642,R126,D417,R137,D73,R1,D711,R880,U718,R104,U444,L36,D974,L360,U12,L890,D337,R184,D745,R164,D931,R915,D999,R452,U221,L399,D761,L987,U562,R25,D642,R411,D605,R964";
-    let input_2 = "L1010,U302,L697,D105,R618,U591,R185,U931,R595,D881,L50,D744,L320,D342,L221,D201,L862,D959,R553,D135,L238,U719,L418,U798,R861,U80,L571,U774,L896,U772,L960,U368,R415,D560,R276,U33,L532,U957,R621,D137,R373,U53,L842,U118,L299,U203,L352,D531,R118,U816,R355,U678,L983,D175,R652,U230,R190,D402,R111,D842,R756,D961,L82,U206,L576,U910,R622,D494,R630,D893,L200,U943,L696,D573,L143,D640,L885,D184,L52,D96,L580,U204,L793,D806,R477,D651,L348,D318,L924,D700,R675,D689,L723,D418,L156,D215,L943,D397,L301,U350,R922,D721,R14,U399,L774,U326,L14,D465,L65,U697,R564,D4,L40,D250,R914,U901,R316,U366,R877,D222,L672,D329,L560,U882,R321,D169,R161,U891,L552,U86,L194,D274,L567,D669,L682,U60,L985,U401,R587,U569,L1,D325,L73,U814,L338,U618,L49,U67,L258,D596,R493,D249,L310,D603,R810,D735,L829,D378,R65,U85,L765,D854,L863,U989,L595,U564,L373,U76,R923,U760,L965,U458,L610,U461,R900,U151,L650,D437,L1,U464,L65,D349,R256,D376,L686,U183,L403,D354,R867,U993,R819,D333,L249,U466,L39,D878,R855,U166,L254,D532,L909,U48,L980,U652,R393,D291,L502,U230,L738,U681,L393,U935,L333,D139,L499,D813,R302,D415,L693,D404,L308,D603,R968,U753,L510,D356,L356,U620,R386,D205,R587,U212,R715,U360,L603,U792,R58,U619,R73,D958,L53,D666,L756,U71,L621,D576,L174,U779,L382,U977,R890,D830,R822,U312,R716,U767,R36,U340,R322,D175,L417,U710,L313,D526,L573,D90,L493,D257,L918,U425,R93,D552,L691,U792,R189,U43,L633,U934,L953,U817,L404,D904,L384,D15,L670,D889,L648,U751,L928,D744,L932,U761,R879,D229,R491,U902,R134,D219,L634,U423,L241";
+    let mut res = vec![];
 
-    let mut path = Path { path: vec![(0, 0)] };
-    let mut path_2 = Path { path: vec![(0, 0)] };
-
-    for i in input.split(',') {
-        path.push(Direction::from(i));
-    }
-    for i in input_2.split(',') {
-        path_2.push(Direction::from(i));
-    }
-
-    let intersections = path.intersections_with(&path_2);
-
-    let steps: Vec<((i32, i32), i32)> = intersections
-        .iter()
-        .map(|i| (*i, path.steps_until(*i) + path_2.steps_until(*i)))
-        .collect();
-
-    let mut min_steps = steps[0];
-    for i in steps {
-        if i.1 < min_steps.1 {
-            min_steps = i;
+    for i0 in 0..10 {
+    for i1 in 0..10 {
+    for i2 in 0..10 {
+    for i3 in 0..10 {
+    for i4 in 0..10 {
+    for i5 in 0..10 {
+        if let Some(_) = Password::new(vec![i0, i1, i2, i3, i4, i5]) {
+            res.push(vec![i0, i1, i2, i3, i4, i5])
         }
-    }
+    }}}}}}
 
-    println!("Steps: {:?}", min_steps);
+    println!("Count: {}", res.len());
 }
 
 #[derive(Debug)]
-struct Path {
-    path: Vec<(i32, i32)>,
+struct Password {
+    digits: [u8; 6],
 }
 
-impl Path {
-    pub fn push(&mut self, dir: Direction) {
-        let starting_x = self.path.last().unwrap().0;
-        let starting_y = self.path.last().unwrap().1;
-
-        match dir {
-            Direction::Up(delta_y) => for i in 0..delta_y as i32 {
-                self.path.push((
-                    starting_x,
-                    starting_y + (i+1)
-                ));
-            }
-            Direction::Down(delta_y) => for i in 0..delta_y as i32 {
-                self.path.push((
-                    starting_x,
-                    starting_y - (i+1),
-                ));
-            }
-            Direction::Right(delta_x) => for i in 0..delta_x as i32 {
-                self.path.push((
-                    starting_x + (i+1),
-                    starting_y,
-                ))
-            }
-            Direction::Left(delta_x) => for i in 0..delta_x as i32 {
-                self.path.push((
-                    starting_x - (i+1),
-                    starting_y,
-               ));
-            }
-        }
-    }
-
-    pub fn intersections_with(&self, other: &Path) -> Vec<(i32, i32)> {
-        println!(
-             "Calculating intersections: iterations: {} * {} = {}",
-             self.path.len(),
-             other.path.len(),
-             self.path.len() * other.path.len()
-        );
-
-        self
-            .path
-            .par_iter()
-            .map(|path_1| {
-                let mut res = vec![];
-
-                for path_2 in &other.path {
-                    if *path_1 == *path_2 {
-                        res.push(*path_1);
-                    }
-                }
-
-                res
-            })
-            .flatten()
-            .filter(|i| *i != (0, 0))
-            .map(|i| i)
-            .collect()
-    }
-
-    pub fn steps_until(&self, point: (i32, i32)) -> i32 {
-        let mut counter = 0;
-
-        for i in &self.path {
-            if *i != point {
-                counter += 1;
-            } else { break };
+impl Password {
+    fn new(from: Vec<u8>) -> Option<Self> {
+        if !(from[0] <= from[1] && from[1] <= from[2] && from[2] <= from[3] && from[3] <= from[4] && from[4] <= from[5]) {
+            return None;
         }
 
-        counter
-    }
+        if !(from[0] == from[1] || from[1] == from[2] || from[2] == from[3] || from[3] == from[4] || from[4] == from[5]) {
+            return None;
+        }
 
+        let number = from[5] as u32 * 10u32.pow(0)
+            + from[4] as u32 * 10u32.pow(1)
+            + from[3] as u32 * 10u32.pow(2)
+            + from[2] as u32 * 10u32.pow(3)
+            + from[1] as u32 * 10u32.pow(4)
+            + from[0] as u32 * 10u32.pow(5);
+        if !(number >= 137683 && number <= 596253) { 
+            return None;
+        }
+
+        let digits = [from[0], from[1], from[2], from[3], from[4], from[5]];
+        Some(Password { digits })
+    }
 }
 
-#[derive(Debug)]
-enum Direction {
-    Up(u32),
-    Down(u32),
-    Left(u32),
-    Right(u32),
-}
+#[test]
+fn test_pw_new() {
+    Password::new(vec![2, 3, 4, 4, 5, 6]).unwrap();
 
-impl Direction {
-    fn from(string: &str) -> Self {
-        let number_text = string.trim_start_matches(char::is_alphabetic);
-        let number = number_text.parse::<u32>().expect("Can't parse number in `Direction::from`");
-
-        match string.chars().nth(0).expect("No chars in `Direction::from` string") {
-            'U' => Direction::Up(number),
-            'D' => Direction::Down(number),
-            'L' => Direction::Left(number),
-            'R' => Direction::Right(number),
-            _ => panic!("Invalid string in `Direction::from`"),
-        }
-    }
+    assert!((0..10).permutations(6).any(|i| i == vec![2, 3, 4, 4, 5, 6]));
 }
