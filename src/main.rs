77e21be58fab54d7da88ac342783d2ed7b4d3cd1
esposm03 <36164633+esposm03@users.noreply.github.com>
2020-10-12 15:05:52 +0200

    Day 4 - Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use itertools::Itertools;
+use std::collections::HashMap;
 
 fn main() {
     let mut res = vec![];
@@ -24,21 +24,29 @@ struct Password {
 
 impl Password {
     fn new(from: Vec<u8>) -> Option<Self> {
+        // Here we check that each digit is greater or equal to the next one.
         if !(from[0] <= from[1] && from[1] <= from[2] && from[2] <= from[3] && from[3] <= from[4] && from[4] <= from[5]) {
+            //println!("Digits <= failed");
             return None;
         }
 
-        if !(from[0] == from[1] || from[1] == from[2] || from[2] == from[3] || from[3] == from[4] || from[4] == from[5]) {
+        // Here we check that there are two equal digits next to each other.
+        let mut digits = HashMap::<u8, u8>::new();
+        for i in &from {
+            digits.insert(*i, *digits.get(i).unwrap_or(&0) + 1);
+        }
+        if !digits.values().any(|val| *val == 2) {
             return None;
         }
 
+        // Here we check that the number is within the range
         let number = from[5] as u32 * 10u32.pow(0)
             + from[4] as u32 * 10u32.pow(1)
             + from[3] as u32 * 10u32.pow(2)
             + from[2] as u32 * 10u32.pow(3)
             + from[1] as u32 * 10u32.pow(4)
             + from[0] as u32 * 10u32.pow(5);
-        if !(number >= 137683 && number <= 596253) { 
+        if number <= 137683 || number >= 596253 { 
             return None;
         }
 
@@ -50,6 +58,4 @@ impl Password {
 #[test]
 fn test_pw_new() {
     Password::new(vec![2, 3, 4, 4, 5, 6]).unwrap();
-
-    assert!((0..10).permutations(6).any(|i| i == vec![2, 3, 4, 4, 5, 6]));
 }
